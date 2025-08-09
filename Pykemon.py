feb2e0c1e5fbefb250f423e55f152610cb8f76f9
Joynal Abedin Abdul PC <joynalabdul2004@gmail.com>
2025-08-09 23:22:41 +0100

    Unpacked Dungeon File, learnt how to set up icon, and how to set up caption.

--- Pykemon.py
+++ Pykemon.py
@@ -1,16 +1,26 @@
 import pygame as pg
 
+#start up game
 pg.init()
+
+#set window size
 h = 1280
 v = 720
-
 display = pg.display.set_mode((h,v))
-pg.mixer.music.load("Assets\Goblins_Dance_(Battle).ogg")
+pg.display.set_caption("Pykemon")
+pg.display.set_icon(pg.image.load("icon.ico"))
+
+#set music
+pg.mixer.music.load("Music\Goblins_Dance_(Battle).ogg")
 pg.mixer.music.play(-1)
+
+#set run mode
 Running = True
 
 
 while Running:
+
+    #set up event loop
     for event in pg.event.get():
         if event.type == pg.QUIT:
             Running = False
